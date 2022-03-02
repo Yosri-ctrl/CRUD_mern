a9093a5bb1a09b0af9e45da06b7560462629fe29
Yosri-ctrl <yosribouabid@gmail.com>
2022-03-02 13:36:03 +0100

    basic files

---
+++ crud_mern/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App container">
+      <h3>Mern Stak app</h3>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
